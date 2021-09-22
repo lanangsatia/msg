4d5589f46bca4400bdbcc3ff981f79ddd1ec386c
Lanang Satia Aji <s.aji@bisma.id>
2021-09-22 15:41:36 +0700

    updt

---
+++ script.js
@@ -0,0 +1,53 @@
+const form = document.getElementById('form');
+const inputName = document.getElementById('name');
+const inputMessage = document.getElementById('message');
+const postData = (url, data) => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (e) {
+        console.log(`Error: ${e}`);
+      });
+  };
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let data = {
+    name: inputName.value,
+    message: inputMessage.value,
+    timestamp: new Date(),
+  };
+
+  console.log(data);
+  postData(
+    'https://sheet.best/api/sheets/280c3557-6f7e-4055-9d3b-22b571749774',data
+  );
+  
+  inputName.value = '';
+  inputMessage.value = '';
+});
+
+
+function getData(){
+  fetch("https://api.apispreadsheets.com/data/18509/").then(res=>{
+    if (res.status === 200){
+      // SUCCESS
+      res.json().then(data=>{
+        const yourData = data
+      }).catch(err => console.log(err))
+    }
+    else{
+      // ERROR
+    }
+  })
+}
+
+
